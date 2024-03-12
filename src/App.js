9db41491dd3bcb02b31717c1b01097dbe430d291
KEYUR <95925706+keyurdungarani@users.noreply.github.com>
2024-03-12 10:28:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ function App() {
         <Route path='/read' element={<Read />}></Route>
         <Route path='/update' element={<Update />}></Route>
         {/* <Index />  */}
+
+        {/* init commit */}
       </Routes>
     </BrowserRouter>
   );
